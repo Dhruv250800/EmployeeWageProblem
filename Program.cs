cde2314c86b85ca2776246c017c508ab3d994a67
DhruvGour <dhruv250800@gmail.com>
2023-05-20 03:32:03 +0530

    [DhruvGour]UC1EmployeePresence

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//UC1 - Check Employee Presence
+
+int presence = -0;
+Random random = new Random();
+
+int check = random.Next(0,2);
+
+if(check == presence)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
