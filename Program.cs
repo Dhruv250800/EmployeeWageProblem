64180593cfd4b198c583aecee35d873a0bcfe0e0
DhruvGour <dhruv250800@gmail.com>
2023-05-20 03:40:43 +0530

    [DhruvGour]-UC2EmployeeWage

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 //UC1 - Check Employee Presence
 
+/*
 int presence = -0;
 Random random = new Random();
 
@@ -15,3 +16,24 @@ else
 {
     Console.WriteLine("Employee is Absent");
 }
+*/
+
+//UC2 - Calculating Employee Wage
+
+int FullTime = 1;
+int hour = 20;
+int empHrs = 0;
+int empWage = 0;
+
+Random random = new Random();
+int empCheck = random.Next(0, 2);
+if(empCheck == FullTime)
+{
+    empHrs = 8;
+}
+else
+{
+    empHrs = 0;
+}
+empWage = empHrs * hour;
+Console.WriteLine("Employee Wage : "+empWage);
