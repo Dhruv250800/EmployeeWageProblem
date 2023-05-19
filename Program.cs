57a8d7b72d6e967f675bf64c7b2ba2cb6014f643
DhruvGour <dhruv250800@gmail.com>
2023-05-20 03:49:40 +0530

    [DhruvGour]-UC3AddPartTime

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ else
 
 //UC2 - Calculating Employee Wage
 
-int FullTime = 1;
+/*int FullTime = 1;
 int hour = 20;
 int empHrs = 0;
 int empWage = 0;
@@ -37,3 +37,27 @@ else
 }
 empWage = empHrs * hour;
 Console.WriteLine("Employee Wage : "+empWage);
+*/
+
+//UC3 - Add Part Time Employee & Wage
+
+int partTime = 1, fullTime = 2, EmpRatePerHour = 20;
+int empHrs = 0;
+int empWage = 0;
+Random random = new Random();
+int empCheck = random.Next(0, 3);
+if(empCheck == partTime)
+{
+    empHrs = 4;
+}
+else if(empCheck == fullTime)
+{
+    empHrs = 8;
+}
+else
+{
+    empHrs = 0;
+}
+
+empWage = empHrs * EmpRatePerHour;
+Console.WriteLine("Emp Wage : "+empWage);
